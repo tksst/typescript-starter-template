49ea3066fa2f0b41c355505d4ef8db8e5cb014d9
Takashi Sato <takashi@tks.st>
2024-07-29 01:02:10 +0900

    ddd

--- tsup.config.lib.mjs
+++ tsup.config.lib.mjs
@@ -2,7 +2,9 @@ import { libOptions } from "@tksst/project-configs/tsup-config.mjs";
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-    ...libOptions,
+    dts: true,
+    format: ["cjs"],
+    outDir: "dist/lib-commonjs/",
     entry: ["src/lib/index.ts"],
 
     // If you know that this library is for Node.js or for a browser, you may want to set the platform.
